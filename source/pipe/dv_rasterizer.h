7de1922edbe0032b1f318921017986005c09b306
thejinchao <thejinchao@gmail.com>
2022-03-19 18:30:37 +0800

    光栅算法分成画线和画三角形两部分

--- source/pipe/dv_rasterizer.h
+++ source/pipe/dv_rasterizer.h
@@ -7,17 +7,19 @@ namespace davinci
 
 class Rasterizer
 {
+	//Draw Line
 public:
 	typedef std::function<void(const std::pair<int32_t, int32_t>&, float)> DrawLineCallback;
-	//Bresenham's line algorithm
-	static void drawLine(int32_t canvasWidth, int32_t canvasHeight, const Vector2& start, const Vector2& end, DrawLineCallback callback);
 
+	//Bresenham's line algorithm
+	static void drawLine(int32_t canvasWidth, int32_t canvasHeight, 
+		const Vector2& start, const Vector2& end, 
+		DrawLineCallback callback);
+
+	//Draw Triangle
 public:
 	typedef std::function<void(const std::pair<int32_t, int32_t>&, const Vector3&)> DrawTriangleCallback;
-	//Scaleline algorithm
-	static void drawTriangleScanline(bool debug, int32_t canvasWidth, int32_t canvasHeight, const Vector3& v0, const Vector3& v1, const Vector3& v2, DrawTriangleCallback callback);
 
-public:
 	struct DebugParam
 	{
 		bool debug;
@@ -28,8 +30,17 @@ public:
 		int32_t fine_id;
 		int32_t edge_id;
 	};
+
 	//Larrabee algorithm
-	static void drawTriangleLarrabee(int32_t canvasWidth, int32_t canvasHeight, const Vector3& v0, const Vector3& v1, const Vector3& v2, DrawTriangleCallback callback, const DebugParam* debug=nullptr);
+	static void drawTriangleLarrabee(int32_t canvasWidth, int32_t canvasHeight, 
+		const Vector3& v0, const Vector3& v1, const Vector3& v2, 
+		DrawTriangleCallback callback, 
+		const DebugParam* debug=nullptr);
+
+	//Scaleline algorithm
+	static void drawTriangleScanline(int32_t canvasWidth, int32_t canvasHeight,
+		const Vector3& v0, const Vector3& v1, const Vector3& v2,
+		DrawTriangleCallback callback);
 };
 
 }
